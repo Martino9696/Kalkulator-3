d1230e3ee18dc5df5eb0a66a4287944fb0f61942
Marcino9696 <252884@edu.p.lodz.pl>
2024-05-21 21:33:51 +0200

    initial commit

---
+++ array.h
@@ -0,0 +1,21 @@
+//
+// Created by kopyt on 21.05.2024.
+//
+
+#ifndef DOWNLOADS_ARRAY_H
+#define DOWNLOADS_ARRAY_H
+#define SIZE 10
+
+void printArray(int array[]);
+
+int findMaximumValue(int array[]);
+
+int findMinimumValue(int array[]);
+
+float calculateAverage(int array[]);
+
+void enterValuesIntoArray(int array[]);
+
+void displayResults(int min, int max, float average);
+
+#endif //DOWNLOADS_ARRAY_H
